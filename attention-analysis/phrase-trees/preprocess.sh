33af5c05c2a4ca695c276c1d8a6bd19a23dfca76
David Mareček <marecek@ufal.mff.cuni.cz>
2018-05-28 13:29:20 +0200

    preprocessing

--- attention-analysis/phrase-trees/preprocess.sh
+++ attention-analysis/phrase-trees/preprocess.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 MOSESDECODER=/home/libovicky/spec_work/mosesdecoder
+EN_TRUECASE=/lnet/troja/projects/emnlp18-nonautoregressive/data/ende/nematus_truecase.en
 
 source ~/troja/nm/bin/activate
 ./preprocess-ptb.py --ptbfiles wsj/23/wsj_*.mrg --trees trees --words words.ptb
 ./ptb2nematus.py --input words.ptb --output words.tok --alignment ali
-cat words.tok | $MOSESDECODER/scripts/recaser/truecase.perl -model nematus_truecase.en > words.nemaprep
+cat words.tok | $MOSESDECODER/scripts/recaser/truecase.perl -model $EN_TRUECASE > words.nemaprep
 cat words.nemaprep | ./wordpieces.py --dictionary /lnet/troja/projects/emnlp18-nonautoregressive/data/ende/wordpieces.txt > words.wps
 
--- attention-analysis/phrase-trees/ptb2nematus.py
+++ attention-analysis/phrase-trees/ptb2nematus.py
@@ -1,19 +1,31 @@
 #!/usr/bin/env python3
 
 import re
+import argparse
+
+ap = argparse.ArgumentParser()
+ap.add_argument("--input", help="PennTreebank tokenization, sentence per line")
+ap.add_argument("--output", help="Output Nematus tokenization, sentence per line")
+ap.add_argument("--alignment", help="Alignment file maping input tokens to output tokens")
+args= ap.parse_args()
+
+output_file = open(args.output, "w")
+alignment_file = open(args.alignment, "w")
 
 with open(args.input) as f:
-    line = f.readline()
-    tokens = line.split(" ")
-    oi = 0
+    lines = f.readlines()
+
+for line in lines:
+    tokens = line.rstrip().split(" ")
     output_tokens = list()
     alignment = list()
     for i in range(len(tokens)):
-        alignment = str(i) + '-' + str(oi) 
-        if (tokens[i] == '``' or tokens[i] == "\'\'"):
+        if (tokens[i] == '``' or tokens[i] == "''"):
             tokens[i] = '&quot;'
-        elif (tokens[i] == '`' or tokens[i] == "\'"):
+        elif (tokens[i] == '`' or tokens[i] == "'"):
             tokens[i] = '&apos;'
+        elif (tokens[i] == '--'):
+            tokens[i] = '-'
         elif (tokens[i] == '-LRB-'):
             tokens[i] = '('
         elif (tokens[i] == '-RRB-'):
@@ -26,21 +38,22 @@ with open(args.input) as f:
             tokens[i] = '{'
         elif (tokens[i] == '-RCB-'):
             tokens[i] = '}'
-        elif (i < len(tokens) - 1 and tokens[i + 1] == "n\'t"):
+        elif (i < len(tokens) - 1 and tokens[i + 1] == "n't"):
             tokens[i] += "n"
-            tokens[i + 1] = "&apos;t"
-        elif (i < len(tokens) - 1 and tokens[i + 1] == "not" \
-              and (tokens[i] == "can" or tokens[i] == "Can")):
-            tokens[i] += "not"
-            alignment = str(i) + '-' + str(oi) + ' ' + str(i+1) + ' ' + str(oi)
-        tokens[i] = re.subn("&", " &apos; ", tokens[i])
-        tokens[i] = re.subn("-", " @-@ ", tokens[i])
-        tokens[i] = re.subn(".$", " .", tokens[i])
-        tokens[i] = re.subn("\s+", " ", tokens[i])
-        tokens[i] = re.subn("^\s+", "", tokens[i])
-        tokens[i] = re.subn("\s+$", "", tokens[i])
+            tokens[i + 1] = "'t"
+        else:
+            tokens[i] = re.sub("&", " &amp; ", tokens[i])
+            tokens[i] = re.sub("'", "&apos;", tokens[i])
+            tokens[i] = re.sub("-", " @-@ ", tokens[i])
+            tokens[i] = re.sub("\.$", " .", tokens[i])
+            tokens[i] = re.sub("\s+", " ", tokens[i])
+            tokens[i] = re.sub("^\s+", "", tokens[i])
+            tokens[i] = re.sub("\s+$", "", tokens[i])
         new_tokens = tokens[i].split(' ')
-        output_tokens.append(new_tokens)
-        output_index += len(new_tokens)
-        for j in range(len(new_tokens))
-            alignment.append(i)
+        output_tokens += new_tokens
+        for j in range(len(new_tokens)):
+            alignment.append(str(i))
+    output_file.write(" ".join(output_tokens) + "\n")
+    alignment_file.write(" ".join(alignment) + "\n")
+
+            
