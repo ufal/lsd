52003bdaa38d4b29ef3ad9bdab369542969c5a7a
David Mareček <marecek@ufal.mff.cuni.cz>
2018-08-10 09:02:08 +0200

    base experiment training

---
+++ attention-analysis/naacl2019/experiments/submit.sh
@@ -0,0 +1,3 @@
+qsub -V -cwd -b y -j y -q gpu-ms.q -l gpu=1,gpu_ram=11G,hostname=dll\* -N encs_16h \
+    source /net/work/people/helcl/virtualenv/tensorflow-1.4-gpu/bin/activate \; \
+    /net/projects/LSD/naacl2019-data/neuralmonkey/bin/neuralmonkey-train encs_16h.ini
