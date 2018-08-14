4816d8c2da653cc336ef866af59e5ab47ca7955f
David Mareček <marecek@ufal.mff.cuni.cz>
2018-08-14 12:43:00 +0200

    glob.glob shuffles data!!!

--- attention-analysis/naacl2019/scripts/concatenate_attentions.py
+++ attention-analysis/naacl2019/scripts/concatenate_attentions.py
@@ -1,17 +1,15 @@
 #!/usr/bin/env python3
 
 import numpy as np
-import argparse
-import glob
-
-ap = argparse.ArgumentParser()
-ap.add_argument("--input", help="input NPZ files with attentions")
-ap.add_argument("--output", help="output NPZ file containing list of attentions")
-args = ap.parse_args()
+import sys
 
 attentions = list()
 
-for filename in glob.glob(args.input):
+input_files = sys.argv[1:]
+output_file = input_files.pop()
+
+for filename in input_files:
+    print(filename)
     x = np.load(filename)
     layers = list()
     for l in range(6):
@@ -19,6 +17,6 @@ for filename in glob.glob(args.input):
         layers.append(att)
     all_layers = np.concatenate(layers, axis=0)
     attentions.append(all_layers)
-np.savez(args.output, *attentions)
+np.savez(output_file, *attentions)
 
 
