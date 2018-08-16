50a3fba2aa0fb2bc6cd466710d90fe949d8a89ae
David Mareček <marecek@ufal.mff.cuni.cz>
2018-08-16 09:37:40 +0200

    drawing heatmaps

--- attention-analysis/naacl2019/scripts/attentions2tree.py
+++ attention-analysis/naacl2019/scripts/attentions2tree.py
@@ -15,6 +15,7 @@ ap = argparse.ArgumentParser()
 ap.add_argument("-a", "--attentions", help="NPZ file with attentions", required=True)
 ap.add_argument("-t", "--tokens", help="Labels (tokens) separated by spaces", required=True)
 ap.add_argument("-d", "--deptrees", help="Output unoriented dep trees into this file")
+ap.add_argument("-h", "--heatmaps", help="Output heatmap prefix")
 ap.add_argument("-e", "--eos", help="Attentions contain EOS", action="store_true")
 args= ap.parse_args()
 
@@ -49,6 +50,58 @@ def deptree(weights, wordpieces):
     lines.append('')
 
     return '\n'.join(lines)
+
+def heatmap(AUC, title, xlabel, ylabel, xticklabels, yticklabels):
+    '''
+    Copied form:
+    https://stackoverflow.com/questions/20574257/constructing-a-co-occurrence-matrix-in-python-pandas
+    '''
+    # Plot it out
+    fig, ax = plt.subplots()    
+    c = ax.pcolor(AUC, edgecolors='k', linestyle= 'dashed', linewidths=0.2, cmap='pink', vmin=0.0, vmax=1.0)
+    
+    # put the major ticks at the middle of each cell
+    ax.set_yticks(np.arange(AUC.shape[0]) + 0.5, minor=False)
+    ax.set_xticks(np.arange(AUC.shape[1]) + 0.5, minor=False)
+    
+    # set tick labels
+    xfont = {'family': 'serif', 'weight': 'normal', 'size': 8, 'rotation' : 'vertical'}
+    yfont = {'family': 'serif', 'weight': 'normal', 'size': 8}
+    #ax.set_xticklabels(np.arange(1,AUC.shape[1]+1), minor=False)
+    ax.set_xticklabels(xticklabels, minor=False, fontdict=xfont)
+    ax.set_yticklabels(yticklabels, minor=False, fontdict=yfont)
+    
+    # set title and x/y labels
+    plt.title(title)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)      
+
+    # Remove last blank column
+    plt.xlim( (0, AUC.shape[1]) )
+
+    # Turn off all the ticks
+    ax = plt.gca()    
+    for t in ax.xaxis.get_major_ticks():
+        t.tick1On = False
+        t.tick2On = False
+    for t in ax.yaxis.get_major_ticks():
+        t.tick1On = False
+        t.tick2On = False
+
+    # Add color bar
+    plt.colorbar(c)
+
+    # Add text in each cell 
+    #show_values(c)
+
+    # Proper orientation (origin at the top left instead of bottom left)
+    ax.invert_yaxis()
+    ax.xaxis.tick_top()
+
+    # resize 
+    fig = plt.gcf()
+    #fig.set_size_inches(cm2inch(40, 20))
+
 
 #load data
 attentions_loaded = np.load(args.attentions)
@@ -103,6 +156,14 @@ for sentence_index in range(sentences_count):
         #print('# sentence', sentence_index)
         #print(tree)
         #print()
+
+    # draw heatmaps
+    # so far only for the first sentence
+    if args.heatmaps and sentence_index == 0:
+        for layer in range(layers_count + 1):
+            # +1 because word_mixture[0] is the initial identity matrix
+            heatmap(word_mixture[layer]), "", "", "", token_list, token_list)
+            plt.savefig(args.heatmaps + str(sentence_index) + '-l' + str(layer) + '.pdf', dpi=300, format='pdf', bbox_inches='tight')
 
 if args.deptrees:
     with open(args.deptrees, 'w') as output:
