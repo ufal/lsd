6778af155a6dc252640f5f66a3533b0384bcb566
Rudolf Rosa <rur@nikde.eu>
2018-10-17 21:42:35 +0200

    LAS graphs fine now

--- attention-analysis/naacl2019/poster/graphs/gnuplot.sh
+++ attention-analysis/naacl2019/poster/graphs/gnuplot.sh
@@ -3,7 +3,9 @@
 t=las
 for l in all br bxr fo hsb hy kk kmr pcm th
 do
-    c=`grep '\t2\t' las_$l.tsv|cut -f1`
+    echo $l
+    c=`grep $'\t2\t' las_$l.tsv|cut -f1`
     sed -e s/LLL/$l/g -e s/CCC/$c/g LAS_LLL_CCC.gnuplot > las_$l.gnuplot
+    gnuplot < las_$l.gnuplot
 done
 
