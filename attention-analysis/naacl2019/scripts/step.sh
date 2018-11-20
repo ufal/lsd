fe47b4222a8a89d40117137a6bd14a87ac36266d
Rudolf Rosa <rosa@ufal.mff.cuni.cz>
2018-11-20 21:53:49 +0100

    updates

--- attention-analysis/naacl2019/scripts/step.sh
+++ attention-analysis/naacl2019/scripts/step.sh
@@ -3,12 +3,19 @@
 b=$1
 c=none
 
+mkdir -p results
+
+function md5 () {
+    echo $1 | md5sum | cut -d' ' -f1
+}
+
 while [ $b != $c ]
 do
 
 c=$b
 s=,$c,
 
+# try removing each head, but also add the full setup
 l=$(echo $c
 for r in ${c//,/ }
 do
@@ -16,19 +23,26 @@ do
     echo ${k:1:-1}
 done)
 
+# run
 for n in $l
 do
     qsub run.shc $n
 done > /dev/null
 
+# wait
 qsub -hold_jid run.shc -sync y -o /dev/null -e /dev/null -b y true > /dev/null
 
-b=$(for n in $l
+# top score and setup
+t=$(for n in $l
 do
-    echo $(cat results/$n) $n
-done | sort -n | tail -n 1 | cut -d' ' -f2)
+    m=$(md5 $n)
+    echo $(cat results/$m) $n
+done | sort -n | tail -n 1)
 
-echo $(cat results/$b) $b
+echo $t
+
+# best setup
+b=${t#* }
 
 done
 
