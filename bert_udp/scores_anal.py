4038f66aa900aa6dda8bd60fbdb969a8e839a88c
Rudolf Rosa <rosa@ufal.mff.cuni.cz>
2019-02-21 15:47:36 +0100

    jak moc se měnej posy

--- bert_udp/scores_anal.py
+++ bert_udp/scores_anal.py
@@ -2,6 +2,7 @@
 
 import sys
 import re
+from collections import defaultdict
 
 # both
 ID = 0
@@ -53,9 +54,9 @@ def readscores(filename):
                     item_id = int(item_id)
                     scores_temp = items[SCORES].split()
                     scores = scores_temp[:item_id]
-                    scores.append('-1')
+                    scores.append('0')
                     scores.extend(scores_temp[item_id:])
-                    scores = ['{: 6.2f}'.format(float(x)) if x != '-1' else '' for x in scores]
+                    scores = [float(x) for x in scores]
                     cur_sent.append(scores)
     return result
 
@@ -68,11 +69,22 @@ scores = readscores(sys.argv[2])
 if (len(conllu) != len(scores)):
     exit("Different number of sentences: " + str(len(conllu)) + " != " + str(len(scores)))
 
+sums = defaultdict(float)
+counts = defaultdict(int)
+
 for sent_poses, sent_scores in zip(conllu, scores):
-    print(*(sent_poses[0]), sep='\t')
-    print(*(sent_poses[1]), sep='\t')
+    poses = sent_poses[0]
     for line in sent_scores:
-        print(*line, sep='\t')
-    print()
+        for score, pos in zip(line, poses):
+            sums[pos] += score
+            counts[pos] += 1
+
+avgs = dict()
+for pos in sums.keys():
+    avgs[pos] = sums[pos]/counts[pos]
+
+poses = sorted(avgs.keys(), key=avgs.get)
+for pos in poses:
+    print(pos, avgs[pos], sep="\t")
 
 
