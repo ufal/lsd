8857fce9ea64d27965fb210f31abfd204942ab8f
Rudolf Rosa <rosa@ufal.mff.cuni.cz>
2019-02-25 00:42:30 +0100

    ujpdate

--- bert_udp/eval_chg_who.py
+++ bert_udp/eval_chg_who.py
@@ -76,31 +76,36 @@ if (len(conllu) != len(scores)):
 
 parents = 0
 grandparents = 0
+grandchildren = 0
 children = 0
 siblings = 0
+roots = 0  # only if nothing else
 others = 0
 total = 0
 for parent, sent_scores in zip(conllu, scores):
     length = len(parent)
-    for child in range(length):
+    for child in range(length):  # child is the changed node
         total += 1
-        most_changed = -1
+        removed_node = -1
         mc_score = -1
         for node in range(length):
             score = sent_scores[(child,node)]
+            #score = sent_scores[(node,child)]
             if score > mc_score:
-                most_changed = node
+                removed_node = node
                 mc_score = score
-        if most_changed == parent[child]:
+        if removed_node == parent[child]:
             parents += 1
-        elif parent[most_changed] == child:
+        elif parent[removed_node] == child:
             children += 1
-        elif parent[child] != -1 and most_changed == parent[parent[child]]:
+        elif parent[child] != -1 and removed_node == parent[parent[child]]:
             grandparents += 1
-        elif parent[child] == parent[most_changed]:
+        elif parent[removed_node] != -1 and child == parent[parent[removed_node]]:
+            grandchildren += 1
+        elif parent[child] == parent[removed_node]:
             siblings += 1
         else:
             others += 1
 
-print('parents', 'grandparents', 'children', 'siblings', 'others', sep='\t')
-print(parents/total, grandparents/total, children/total, siblings/total, others/total, sep='\t')
+print('parent', 'child', 'sibling', 'grandparent', 'grandchild', 'other', sep='\t')
+print(parents/total, children/total, siblings/total, grandparents/total, grandchildren/total, others/total, sep='\t')
