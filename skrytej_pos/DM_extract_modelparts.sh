11d8c59afd122efd8f4be19b0b4ad08ca5dcb1e6
Rudolf Rosa <rosa@ufal.mff.cuni.cz>
2019-05-15 15:50:19 +0200

    configurable training

---
+++ skrytej_pos/DM_extract_modelparts.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+extract_model_part_from_ckpt.py /net/projects/LSD/summarization/models/sumeczech-rnn-rnn/variables.data inpseq tagger-summeczech-rnn-rnn/embeddings.ckpt
+extract_model_part_from_ckpt.py /net/projects/LSD/summarization/models/sumeczech-rnn-rnn/variables.data encoder_text tagger-summeczech-rnn-rnn/encoder.ckpt
+#extract_model_part_from_ckpt.py /net/projects/LSD/sentiment/models/csfd_rnn/variables.data text_rnn_input tagger-sentiment-csfd-rnn/embeddings.ckpt
+#extract_model_part_from_ckpt.py /net/projects/LSD/sentiment/models/csfd_rnn/variables.data text_rnn tagger-sentiment-csfd-rnn/encoder.ckpt
+#extract_model_part_from_ckpt.py /net/projects/LSD/sentiment/models/csfd_rnn_san/variables.data text_rnn_input tagger-sentiment-csfd-rnn-san/embeddings.ckpt
+#extract_model_part_from_ckpt.py /net/projects/LSD/sentiment/models/csfd_rnn_san/variables.data text_rnn tagger-sentiment-csfd-rnn-san/encoder.ckpt
+
+
