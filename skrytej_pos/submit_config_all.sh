de6241309aa66e85a5261c30fa896f0cc8b91e36
Rudolf Rosa <rosa@ufal.mff.cuni.cz>
2019-05-17 17:40:07 +0200

    even more configuralbe

--- skrytej_pos/submit_config_all.sh
+++ skrytej_pos/submit_config_all.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+c=$1
+
 for s in split1 split2
 do
     for l in 1 5 10 50 100 500 1000 5000 10000 30000
     do
-        qsub -N te-$s-$l tagger-SSS-LLL-HHH.shc $s $l embsonly
+        qsub -N t-$c-$s-$l tagger-SSS-LLL-HHH.shc $s $l $c
     done
 done
 
