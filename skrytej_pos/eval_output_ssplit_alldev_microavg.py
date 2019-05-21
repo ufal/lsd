641c1d77435264c5c366abfffe69540435c20b72
Rudolf Rosa <rosa@ufal.mff.cuni.cz>
2019-05-21 18:11:56 +0200

    evalmicro

---
+++ skrytej_pos/eval_output_ssplit_alldev_microavg.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python3
+#coding: utf-8
+
+import sys
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+def readtrain(filename):
+    trainwords = set()
+    with open(filename) as fh:
+        for line in fh:
+            words = line.split()
+            trainwords.update(words)
+    return trainwords
+
+def readtest(filename):
+    with open(filename) as fh:
+        line = fh.read()
+        words = line.split()
+        testwords = set(words)
+    return testwords
+
+def evalpreds(infile, predfile, goldfile, testwords):
+    total = 0
+    correct = 0
+    with open(infile) as ifh, open(predfile) as pfh, open(goldfile) as gfh:
+        for words, preds, golds in zip(ifh, pfh, gfh):
+            words = words.split()
+            preds = preds.split()
+            golds = golds.split()
+            for word, pred, gold in zip(words, preds, golds):
+                #if gold != 'PUNCT':
+                    if word in testwords:
+                        total += 1
+                        if pred == gold:
+                            correct += 1
+    return total, correct
+
+
+
+
+if len(sys.argv) != 8:
+    print('Usage:')
+    print(sys.argv[0],
+            'cs-ud-dev.forms',
+            'cs-ud-dev.tags',
+            'output/cs-ud-dev.tagger-hidden-new-ssplit',
+            '-30000.output',
+            'train/cs-ud-train.forms.ssplit',
+            '.30000',
+            'cs-ud-dev.invocabforms',
+            )
+    exit()
+
+infile, goldfile, predfile_pref, predfile_suf, trainfile_pref, trainfile_suf, testwordsfile = sys.argv[1:8]
+
+testwords = readtest(testwordsfile)
+
+total_seen = 0
+correct_seen = 0
+total_unseen = 0
+correct_unseen = 0
+for ssplit1 in range(0, 10, 2):
+
+    ssplit2 = ssplit1 + 1
+
+    trainwords1 = readtrain(trainfile_pref + str(ssplit1) + trainfile_suf)
+    trainwords2 = readtrain(trainfile_pref + str(ssplit2) + trainfile_suf)
+    
+    testwordsA = trainwords1.difference(trainwords2).intersection(testwords)
+    testwordsB = trainwords2.difference(trainwords1).intersection(testwords)
+    
+    predfile1 = predfile_pref + str(ssplit1) + predfile_suf
+    predfile2 = predfile_pref + str(ssplit2) + predfile_suf
+    
+    t1_seen, c1_seen = evalpreds(infile, predfile1, goldfile, testwordsA)
+    t2_seen, c2_seen = evalpreds(infile, predfile2, goldfile, testwordsB)
+
+    t1_unseen, c1_unseen = evalpreds(infile, predfile1, goldfile, testwordsB)
+    t2_unseen, c2_unseen = evalpreds(infile, predfile2, goldfile, testwordsA)
+    
+    total_seen += t1_seen + t2_seen
+    correct_seen += c1_seen + c2_seen
+    total_unseen += t1_unseen + t2_unseen
+    correct_unseen += c1_unseen + c2_unseen
+
+    logging.info(' '.join(['ssplit', str(ssplit1), 'seen', str(c1_seen), '/', str(t1_seen), '=', str(c1_seen/t1_seen) ]) )
+    logging.info(' '.join(['ssplit', str(ssplit2), 'unsn', str(c2_unseen), '/', str(t2_unseen), '=', str(c2_unseen/t2_unseen) ]) )
+    logging.info(' '.join(['ssplit', str(ssplit2), 'seen', str(c2_seen), '/', str(t2_seen), '=', str(c2_seen/t2_seen) ]) )
+    logging.info(' '.join(['ssplit', str(ssplit2), 'unsn', str(c2_unseen), '/', str(t2_unseen), '=', str(c2_unseen/t2_unseen) ]) )
+
+
+print('SEEN', correct_seen, '/', total_seen, '=', (correct_seen/total_seen) )
+print('UNSEEN', correct_unseen, '/', total_unseen, '=', (correct_unseen/total_unseen) )
+
+
+
